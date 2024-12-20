981a8c8d23d3d0d6cbf894cf71b1698683a022b5
AkashNaik11 <naikaaakash7735@gmail.com>
2024-12-20 23:43:35 +0530

    Added portfolio section to the page

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const resumeLists= document.querySelectorAll('.resume-list');
-const resumeBoxes=document.querySelectorAll('.resume-box')
+const resumeBoxes=document.querySelectorAll('.resume-box');
+const portfolioLists= document.querySelectorAll('.portfolio-list');
+const portfolioBoxes=document.querySelectorAll('.portfolio-box');
 
 
 
@@ -12,6 +14,16 @@ resumeLists.forEach((list,idx)=>{
 
         document.querySelector('.resume-box.active').classList.remove('active');
         resumeBoxes[idx].classList.add('active');
+    })
+});
+
+portfolioLists.forEach((list,idx)=>{
+    list.addEventListener('click',()=>{
+        document.querySelector('.portfolio-list.active').classList.remove('active');
+        list.classList.add('active');
+
+        document.querySelector('.portfolio-box.active').classList.remove('active');
+        portfolioBoxes[idx].classList.add('active');
     })
 });
 
